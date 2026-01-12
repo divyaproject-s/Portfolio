7c7d633f41a99819ee5f5ef3f8ed090dd6a66d18
divyaproject-s <divyasubramanian1103@gmail.com>
2026-01-12 13:39:45 +0530

    To add a new features

--- script.js
+++ script.js
@@ -6,7 +6,38 @@ let isDeleting = false;
 
 function typeEffect(){
     if(i >= words.length) i = 0;
+    current = words[i];let words = ["Full Stack Developer", "Web Designer", "Python Developer", "Blockchain Developer"];
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function typeEffect(){
+    if(i >= words.length) i = 0;
     current = words[i];
+
+    if(!isDeleting){
+        document.getElementById("typing").innerHTML = current.substring(0, j++);
+        if(j == current.length){
+            isDeleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    } else {
+        document.getElementById("typing").innerHTML = current.substring(0, j--);
+        if(j == 0){
+            isDeleting = false;
+            i++;
+        }
+    }
+    setTimeout(typeEffect, 120);
+}
+typeEffect();
+
+function showMsg(){
+    alert("Thank you Divya! I will contact you soon 🚀");
+}
+
 
     if(!isDeleting){
         document.getElementById("typing").innerHTML = current.substring(0, j++);
