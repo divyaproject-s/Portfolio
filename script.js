61db48f2940630c110266e76d59aa94d8603c7ae
divyaproject-s <divyasubramanian1103@gmail.com>
2026-01-18 23:05:34 +0530

    To update the layout complete 90 percent

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-let words = ["Full Stack Developer", "Python Developer", "Web Designer"];
-let i = 0, j = 0;
-let deleting = false;
+let words = ["Python Full Stack Developer", "MCA Student", "Web Developer"];
+let i = 0, j = 0, deleting = false;
 
-function typeEffect(){
+function typeEffect() {
   let word = words[i];
   document.getElementById("typing").innerText = word.substring(0, j);
 
-  if(!deleting){
+  if (!deleting) {
     j++;
-    if(j === word.length + 1){
+    if (j === word.length + 1) {
       deleting = true;
       setTimeout(typeEffect, 1000);
       return;
     }
   } else {
     j--;
-    if(j === 0){
+    if (j === 0) {
       deleting = false;
       i = (i + 1) % words.length;
     }
